82328a4985dadf2ea68d1d869e1f9f743325fe70
moitank <marcelo.moita@al.resilia.com.br>
2021-02-25 15:05:10 -0300

    Primeiro commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <main className="MainCapa">
+          <h1>Bons Drinks</h1>
+        </main>
+      </div>
+    );
+  }
+}
